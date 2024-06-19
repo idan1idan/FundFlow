d5e82ca7deb52a009ea7c3f8825c40615f7448df
Idan <gigiidan@gmail.com>
2024-06-19 22:43:41 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className='container mx-auto'>
+      
+    </div>
+    
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
+  theme: {
+    backgroundColor: "",
+    screens: {
+      mobile: "480px",
+      tablet: "768px",
+      laptop: "1280px",
+      desktop: "1400px",
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        desktop: "1400px",
+      },
+    },
+    fontFamily: {
+      sans: ["Open-sans", "sans-serif"],
+    },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
